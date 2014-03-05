e8bb186b537dd70e8b39aa5434d27ffa0a671376
Mark Nunberg <mnunberg@haskalah.org>
2014-03-05 11:47:50 -0500

    Move variables into 'vars.sh'

--- common/vars.sh
+++ common/vars.sh
@@ -1,6 +1,21 @@
+# The local directory to server as the repository root
 export PREFIX=/repos/
-#export MASTER_HOST=jenkins@sdkbuilds.couchbase.com
+
+# This indicates the master is local, not remote
 export MASTER_IS_LOCAL=1
-export GPG_KEY=D9223EDA
+
+# GPG key for the _repository_
+export APT_GPG_KEY=D9223EDA
+
+# GPG Key for dpkg/debsign
+export DPKG_GPG_KEY=79CF7903
+
+# Architectures for debian
+export DEB_ARCHES="amd64 i386"
+export QUICK_DEB_ARCHES="amd64"
+
+export DEB_DISTROS="lucid oneiric precide"
+export QUICK_DEB_DISTROS=lucid
+
 export RPM_GPG_KEY=$GPG_KEY
 mkdir -p $PREFIX
--- deb/build-deb-cowbuilders.sh
+++ deb/build-deb-cowbuilders.sh
@@ -2,7 +2,11 @@
 set -x
 set -e
 
-PARSE_SCRIPT=$(dirname $0)/../git-describe-parse/parse-git-describe.pl
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+
+PARSE_SCRIPT=$SCRIPTPATH/../git-describe-parse/parse-git-describe.pl
 if [ $(echo "$@" | grep -q quick) ]
 then
     QUICK=1
@@ -13,21 +17,25 @@ VERSION=$($PARSE_SCRIPT --tar)
 DEB_VERSION=$($PARSE_SCRIPT --deb)
 WORKSPACE=$PWD/build
 PKGDIR=$WORKSPACE/libcouchbase-$VERSION
-GPG_KEY=79CF7903
 
 mkdir -p $PKGDIR
 cp -r packaging/deb $PKGDIR/debian
 cp libcouchbase-$VERSION.tar.gz $WORKSPACE/libcouchbase_$DEB_VERSION.orig.tar.gz
+
+# Extract the package
 (
-cd $WORKSPACE;
-tar zxf libcouchbase_$DEB_VERSION.orig.tar.gz
+    cd $WORKSPACE;
+    tar zxf libcouchbase_$DEB_VERSION.orig.tar.gz
 )
 
+# Sign the source package
 if [ -z "${NO_GPG}" ]
 then
-    PKG_GPG_OPTS="--debbuildopts -k$GPG_KEY"
-    SRC_GPG_OPTS="-k $GPG_KEY"
+    PKG_GPG_OPTS="--debbuildopts -k$DPKG_GPG_KEY"
+    SRC_GPG_OPTS="-k $DPKG_GPG_KEY"
 fi
+
+# Generate the debianized source
 (
     cd $PKGDIR;
     dch \
@@ -35,19 +43,20 @@ fi
         --newversion "$DEB_VERSION" \
         "Release package for libcouchbase $DEB_VERSION"
     
-    dpkg-buildpackage -rfakeroot -d -S -sa -k$GPG_KEY
+    dpkg-buildpackage -rfakeroot -d -S -sa -k$DPKG_GPG_KEY
 )
+
 
 mv $WORKSPACE/*.{dsc,tar.gz} $PWD
 rm -rf $WORKSPACE
 
 if [ -n "$QUICK" ]
 then
-    DISTS=lucid
-    ARCHES=i386
+    DISTS=$QUICK_DEB_DISTROS
+    ARCHES=$QUICK_DEB_ARCHES
 else
-    DISTS="lucid oneiric precise"
-    ARCHES="i386 amd64"
+    DISTS=$DEB_DISTROS
+    ARCHES=$DEB_ARCHES
 fi
 
 for DIST in $DISTS; do
@@ -62,7 +71,7 @@ for DIST in $DISTS; do
             --debbuildopts "-us -uc" \
             libcouchbase_$DEB_VERSION.dsc
         if [ -z "$NO_GPG" ]
-        then debsign -k $GPG_KEY --no-re-sign \
+        then debsign -k $DPKG_GPG_KEY --no-re-sign \
             $RESDIR/*$ARCH*.changes $RESDIR/*.dsc
         fi
     done
