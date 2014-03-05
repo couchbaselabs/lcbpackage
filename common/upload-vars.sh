2bcda6719893db04d3166b75b94c3946a3290111
Mark Nunberg <mnunberg@haskalah.org>
2014-03-05 10:40:44 -0500

    Change APT GPG key

--- common/upload-vars.sh
+++ common/upload-vars.sh
@@ -1,6 +1,6 @@
 export PREFIX=/repos/
 #export MASTER_HOST=jenkins@sdkbuilds.couchbase.com
 export MASTER_IS_LOCAL=1
-export GPG_KEY=79CF7903
+export GPG_KEY=D9223EDA
 export RPM_GPG_KEY=$GPG_KEY
 mkdir -p $PREFIX
