1e467c1ee36ee2834627feca687fd455e5e1c3b7
Mark Nunberg <mnunberg@haskalah.org>
2014-06-23 20:36:54 -0700

    Add a little script to download the windows binaries from jenkins

---
+++ download_windows_binaries.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+ARCHES="x86 amd64"
+VCVERS="9 10 11"
+URIBASE="http://sdkbuilds.couchbase.com/job/lcb-win32-cmake"
+LCBVERS=$1
+
+for arch in $ARCHES; do
+    for vers in $VCVERS; do
+        for hostarch in 64 32; do
+            url="$URIBASE/ARCH=$arch,MSVCC_VER=$vers,label=sdk_win2008_${hostarch}/ws/BUILD"
+            url="${url}/libcouchbase-${LCBVERS}_${arch}_vc${vers}.zip"
+            echo $url
+            wget --no-verbose "$url"
+        done
+    done
+done
