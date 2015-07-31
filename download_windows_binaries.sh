89d816d87cf950bb645e8bb2fc4c3f694ed9421d
Mark Nunberg <mnunberg@haskalah.org>
2015-07-31 10:09:16 -0700

    Modify windows download script for new builders

--- download_windows_binaries.sh
+++ download_windows_binaries.sh
@@ -6,11 +6,9 @@ LCBVERS=$1
 
 for arch in $ARCHES; do
     for vers in $VCVERS; do
-        for hostarch in 64 32; do
-            url="$URIBASE/ARCH=$arch,MSVCC_VER=$vers,label=sdk_win2008_${hostarch}/ws/BUILD"
-            url="${url}/libcouchbase-${LCBVERS}_${arch}_vc${vers}.zip"
-            echo $url
-            wget "$url"
-        done
+        url="$URIBASE/ARCH=$arch,MSVCC_VER=$vers,label=win-2008/ws/BUILD"
+        url="${url}/libcouchbase-${LCBVERS}_${arch}_vc${vers}.zip"
+        echo $url
+        wget "$url"
     done
 done
